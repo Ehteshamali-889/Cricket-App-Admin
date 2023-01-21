b58fc7313fe60986236f8ba19107622284772c4e
Ehtesham Ali <63139751+Ehteshamali-889@users.noreply.github.com>
2023-01-21 10:27:29 +0500

    Add files via upload

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import User from '../User/User'
+
+const index = () => {
+  return (
+	<div>
+        <Header />
+        <div>
+            <User />
+        </div>
+    </div>
+  )
+}
+
+export default index
